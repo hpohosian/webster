f4c8516831fc986e2d1990a4d3957e75f31b2e52
herman-nott <mcgerman2006@gmail.com>
2026-05-22 23:11:20 +0200

    add debug loggs

--- webster-backend/src/auth/auth.service.ts
+++ webster-backend/src/auth/auth.service.ts
@@ -70,6 +70,10 @@ export class AuthService {
     // тут будет сохранение verification кода
     await this.emailVerificationService.create(savedUser.id, code, expiresAt);
 
+    console.log('RESEND KEY:', process.env.RESEND_API_KEY);
+
+    console.log('TRY SEND EMAIL');
+
     // отправка email будет отдельным сервисом
     await this.mailService.sendVerification(savedUser.email, code);
 
