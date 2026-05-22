62a75f00b31c86dc0ce4cb7748580f12e9c772e0
herman-nott <mcgerman2006@gmail.com>
2026-05-22 22:20:06 +0200

    ipv6 -> ipv4 in mail functions

--- webster-backend/src/mail/mail.service.ts
+++ webster-backend/src/mail/mail.service.ts
@@ -9,6 +9,7 @@ export class MailService {
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false,
+    family: 4,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
