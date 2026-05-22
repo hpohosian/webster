0f8c49037d4f11c11e8265dc1909ad3dc8ed17d2
herman-nott <mcgerman2006@gmail.com>
2026-05-22 23:23:28 +0200

    add debug loggs

--- webster-backend/src/auth/auth.service.ts
+++ webster-backend/src/auth/auth.service.ts
@@ -95,6 +95,9 @@ export class AuthService {
     user.isEmailConfirmed = true;
     await this.userService.save(user);
 
+    console.log('user.isEmailConfirmed', user.isEmailConfirmed);
+    
+
     await this.emailVerificationService.deleteByUserId(user.id);
 
     return { message: 'Email successfully confirmed' };
